2ffd42430fe4c9eb9dcfa6ba0af2059519d4d7bc
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-05-29 13:52:32 +0300

    Update اسم_البوت.py

--- ZeMusic/plugins/models/اسم_البوت.py
+++ ZeMusic/plugins/models/اسم_البوت.py
@@ -4,6 +4,7 @@ from strings.filters import command
 import random
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import BOT_NAME
 
 italy = ["لبيه يا {nameuser}",
          "ءامر يا {nameuser}",
@@ -11,11 +12,14 @@ italy = ["لبيه يا {nameuser}",
          "شفيك",
          "شتبي مني",
          "زعلان",
-         "مريض"]
+         "{BOT_NAME} يلعنك يا {nameuser}",
+         "ايش 😒",
+         "يارب يكون شي مهم",
+         "حوت يكلك"]
 
 @app.on_message(filters.text & filters.regex(r"(^|\W)بوت(\W|$)"))
 async def Italymusic(client, message):
     if "بوت" in message.text:
         response = random.choice(italy)
-        response = response.format(nameuser=message.from_user.first_name)
+        response = response.format(nameuser=message.from_user.first_name, BOT_NAME=BOT_NAME)
         await message.reply(response)
