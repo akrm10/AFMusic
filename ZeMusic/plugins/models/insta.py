21f7f4ef5892cfcfddc716cd14b93fa69d169518
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-05-31 20:52:54 +0300

    Update insta.py

--- ZeMusic/plugins/models/insta.py
+++ ZeMusic/plugins/models/insta.py
@@ -16,7 +16,7 @@ async def download_instareels(c: Client, m: Message):
         return
 
     ydl_opts = {
-         outtmpl :  downloads/%(title)s.%(ext)s 
+        'outtmpl' : 'downloads/%(title)s.%(ext)s'
     }
 
     try:
