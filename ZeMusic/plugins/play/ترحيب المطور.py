919c34bdbf6c9ed74b32dd546b435bfb73619fe3
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-06-14 01:51:37 +0300

    Update ترحيب المطور.py

--- ZeMusic/plugins/play/ترحيب المطور.py
+++ ZeMusic/plugins/play/ترحيب المطور.py
@@ -12,13 +12,13 @@ from pyrogram import filters
 @app.on_message(filters.new_chat_members)
 async def wel__come(client: Client, message: Message):
 	chatid= message.chat.id
-	await client.send_message(text=f"- نورت ياا فرتكهه😘🤝️ {message.from_user.mention}\n│ \n└ʙʏ في {message.chat.title}",chat_id=chatid)
+	await client.send_message(text=f"- انظم المطور {message.from_user.mention}\n│ \n└ʙʏ في {message.chat.title}",chat_id=chatid)
 
 
 @app.on_message(filters.left_chat_member)
 async def good_bye(client: Client, message: Message):
 	chatid= message.chat.id
-	await client.send_message(text=f"- مشيت ليه يوحش يلا بسلامات🥲👋\n│ \n└ʙʏ  {message.from_user.mention} ",chat_id=chatid)
+	await client.send_message(text=f"- غادر المطور\n│ \n└ʙʏ  {message.from_user.mention} ",chat_id=chatid)
 
 
 
