c99e953545aa373f041f0d1dfa97f3dbe9c6e9d2
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-06-14 05:57:39 +0300

    Update ترحيب المطور.py

--- ZeMusic/plugins/play/ترحيب المطور.py
+++ ZeMusic/plugins/play/ترحيب المطور.py
@@ -6,18 +6,13 @@ import os
 
 
 @app.on_chat_member_updated(filters=lambda _, response: response.new_chat_member, group=847)
-async def WelcomeDev(_, response: ChatMemberUpdated, message):
+async def WelcomeDev(_, response: ChatMemberUpdated):
     dev_id = OWNER_ID # حط ايديك هنا
     if response.from_user.id == dev_id:
         info = await app.get_chat(dev_id)
         name = info.first_name
         bio = info.bio
         
-        await message.reply(
+        await app.send_message(
             chat_id=response.chat.id,
-            text=f"⟡ انظم المطور <a href= tg://user?id={dev_id} >{name}</a>\n⟡ {bio}")
-
-        
-        #await app.send_message(
-            #chat_id=response.chat.id,
-            #text=f"⟡ انظم المطور <a href='tg://user?id={dev_id}'>{name}</a>\n⟡ {bio}")
+            text=f"⟡ انظم المطور <a href='tg://user?id={dev_id}'>{name}</a>\n⟡ {bio}")
