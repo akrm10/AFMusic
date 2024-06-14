3fc97585f5d05db7ae71715c7a360b2712640c4a
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-06-14 05:35:59 +0300

    Update ترحيب المطور.py

--- ZeMusic/plugins/play/ترحيب المطور.py
+++ ZeMusic/plugins/play/ترحيب المطور.py
@@ -1,12 +1,13 @@
 from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton
 from ZeMusic import app
+from config import OWNER_ID
 import os
 
 
 @app.on_chat_member_updated(filters=lambda _, response: response.new_chat_member, group=847)
 async def WelcomeDev(_, response: ChatMemberUpdated):
-    dev_id = 6600943153 # حط ايديك هنا
+    dev_id = OWNER_ID # حط ايديك هنا
     if response.from_user.id == dev_id:
         info = await app.get_chat(dev_id)
         name = info.first_name
@@ -15,4 +16,3 @@ async def WelcomeDev(_, response: ChatMemberUpdated):
         await app.send_message(
             chat_id=response.chat.id,
             text=f"⟡ انظم المطور <a href='tg://user?id={dev_id}'>{name}</a> دخل الجروب.\n⟡ {bio}")
-    
